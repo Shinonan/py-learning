69ccc9109a30361404fe62b95ca8114da899ee5a
Shinonan <sayo1luvuwu@gmail.com>
2024-10-20 23:57:34 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+mid = len(list_players) // 2
+first_team = list_players[:mid]
+second_team = list_players[mid:]
+print(first_team)
+print(second_team)
